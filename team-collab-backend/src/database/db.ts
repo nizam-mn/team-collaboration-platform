2f537ea580853acb1a625fc7055512b96431712f
Mohammed Nizam <mohammednizam2404@gmail.com>
2026-04-03 09:54:59 +0530

    updates for hosting

--- team-collab-backend/src/database/db.ts
+++ team-collab-backend/src/database/db.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // 🔥 REQUIRED for Neon
+    rejectUnauthorized: false,
   },
 });
 
