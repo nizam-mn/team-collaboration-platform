7ef4d1000c5e97c813e17a2485c9724030b45998
Mohammed Nizam <mohammednizam2404@gmail.com>
2026-04-03 20:07:25 +0530

    updates for hosting

--- team-collab-backend/src/notes/notes.module.ts
+++ team-collab-backend/src/notes/notes.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
 imports: [AuthModule],
